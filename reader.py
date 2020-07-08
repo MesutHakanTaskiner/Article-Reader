9a3de99c8fc94d3f1e59ca630873e88a07a7e4a9
HakanTaskinerr <HakanTaskinerr@users.noreply.github.com>
2020-07-08 15:11:23 +0300

    Update reader.py

--- reader.py
+++ reader.py
@@ -4,12 +4,9 @@ from bs4 import BeautifulSoup
 import wikipedia
 
 main_page = Tk()
-root = Tk()
 
-root.title('Wikipedia')
-root.geometry("350x40")
+main_page.title('Reader')
 
-mylabel = Label(root, text = "")
 
 def summary():
     global mylabel
@@ -27,26 +24,40 @@ def summary():
 
     topic.delete(0 , END)
 
+def select():
+    
+    root = Tk()
+    root.title('Wikipedia')
+    root.geometry("350x40")
+
+
+    topic = Entry(root, width = 20)
+    topic.grid(row = 0, column = 0, ipady = 5)
+    topic.place(x = 0, y = 3)
+
+    mybutton = Button(root, text = "Enter your topic what dou you want to read", command = summary)
+    mybutton.grid(row = 0, column = 1)
+    mybutton.place(x = 100, y = 0)
+
+def random():
+    return
+
+
+    
+
 #r = requests.get("https://tr.wikipedia.org/wiki/Google")
 
 #soup = BeautifulSoup(r.content, "html.parser")
 
-main_page_label = Label(main_page, text = "Welcome To The Our Reader")
+main_page_label = Label(main_page, text = "Welcome To The My Reader")
 main_page_label.grid(row = 0, column = 0)
+main_page_label.config(font=("Courier", 20))
 
-main_page_button = Button(main_page, text = "If You Want To Select Your Topic Click It")
+main_page_button = Button(main_page, text = "If You Want To Select Your Topic Click It", command = select)
 main_page_button.grid(row = 1, column = 0, pady = 10)
 
-main_page_button2 = Button(main_page, text = "If You Want A Random Topic Click It")
+main_page_button2 = Button(main_page, text = "If You Want A Random Topic Click It", command = random)
 main_page_button2.grid(row = 2, column = 0, pady = 10, ipadx = 10)
 
-topic = Entry(root, width = 20)
-topic.grid(row = 0, column = 0, ipady = 5)
-topic.place(x = 0, y = 3)
 
-mybutton = Button(root, text = "Enter your topic what dou you want to read", command = summary)
-mybutton.grid(row = 0, column = 1)
-mybutton.place(x = 100, y = 0)
-
-
-root.mainloop()
+main_page.mainloop()
