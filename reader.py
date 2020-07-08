bb44d9600a5ae14cd4631615256497d8be36aa9f
HakanTaskinerr <HakanTaskinerr@users.noreply.github.com>
2020-07-08 16:12:27 +0300

    Update reader.py

--- reader.py
+++ reader.py
@@ -33,26 +33,43 @@ def select():
 
 
 def random():
-    global mylabel
-    global mybutton
+    global random_label
     global random_window
+    global random_label_2
     
     random_window = Toplevel()
     random_window.title('Random Topic')
+    random_window.geometry("170x30")
 
-    mylabel = Label(random_window, text = "")
+    random_label = Label(random_window, text = "")
+    random_label_2 = Label(random_window, text = "")
 
     random_button = Button(random_window, text = "Click It For Random Topic", command = lambda : random_select())
     random_button.grid(row = 0, column = 0)
     random_button.place(x = 10, y = 0)
 
 def random_select():
-    global mylabel
+    global random_label
     global random_window
+    global random_label_2
 
-    #mylabel = Label(random_window, text = wikipedia.summary(, sentences = 20), wraplength = 400)
-    #mylabel.grid(row = 1, column = 0)
-    #mylabel.place(x = 50, y = 50)
+    random_window.geometry("500x500")
+
+    topic = wikipedia.random()
+
+    random_label.grid_forget()
+    random_label.destroy()
+
+    random_label_2.grid_forget()
+    random_label_2.destroy()
+
+    random_label = Label(random_window, text = "Topic : " + topic)
+    random_label.grid(row = 1, column = 0)
+    random_label.place(x = 160, y = 20)
+
+    random_label_2 = Label(random_window, text = wikipedia.summary(topic, sentences = 20), wraplength = 400)
+    random_label_2.grid(row = 1, column = 0)
+    random_label_2.place(x = 50, y = 50)
 
     exit_button = Button(random_window, text = "Exit.", command = random_window.destroy)
     exit_button.grid(row = 2, column = 0)
@@ -63,11 +80,8 @@ def summary():
     global topic
     global root
 
-    mylabel.grid_forget()#
+    mylabel.grid_forget()
     mylabel.destroy()
-            
-    topic.get()
-    #r = requests.get("https://tr.wikipedia.org/wiki/" + topic.get() + "")
             
     root.geometry("500x500")
 
@@ -81,11 +95,6 @@ def summary():
 
     topic.delete(0 , END)
 
-    
-
-#r = requests.get("https://tr.wikipedia.org/wiki/Google")
-
-#soup = BeautifulSoup(r.content, "html.parser")
 
 main_page_label = Label(main_page, text = "Welcome To The My Reader")
 main_page_label.grid(row = 0, column = 0)
@@ -97,5 +106,7 @@ main_page_button.grid(row = 1, column = 0, pady = 10)
 main_page_button2 = Button(main_page, text = "If You Want A Random Topic Click It", command = random)
 main_page_button2.grid(row = 2, column = 0, pady = 10, ipadx = 10)
 
+exit_button = Button(main_page, text = "Exit.", command = main_page.quit)
+exit_button.grid(row = 3, column = 1, ipadx = 20)
 
 main_page.mainloop()
