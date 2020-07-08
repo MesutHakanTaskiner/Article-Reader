24e2e1d8aba7a35a771838ce3af168e659f5e0d1
HakanTaskinerr <HakanTaskinerr@users.noreply.github.com>
2020-07-08 15:30:44 +0300

    Update reader.py

--- reader.py
+++ reader.py
@@ -8,40 +8,54 @@ main_page = Tk()
 main_page.title('Reader')
 
 
-def summary():
-    global mylabel
-
-    mylabel.grid_forget()
-    mylabel.destroy()
-    
-    topic.get()
-    #r = requests.get("https://tr.wikipedia.org/wiki/" + topic.get() + "")
-    
-    root.geometry("500x500")
-    mylabel = Label(root, text = wikipedia.summary(topic.get(), sentences = 20), wraplength = 400)
-    mylabel.grid(row = 1, column = 0)
-    mylabel.place(x = 50, y = 50)
-
-    topic.delete(0 , END)
-
 def select():
+    global mylabel
+    global topic
+    global mybutton
+    global root
     
-    root = Tk()
+    root = Toplevel()
     root.title('Wikipedia')
     root.geometry("350x40")
+
+
+    mylabel = Label(root, text = "")
 
 
     topic = Entry(root, width = 20)
     topic.grid(row = 0, column = 0, ipady = 5)
     topic.place(x = 0, y = 3)
 
-    mybutton = Button(root, text = "Enter your topic what dou you want to read", command = summary)
+    mybutton = Button(root, text = "Enter your topic what dou you want to read", command = lambda : summary())
     mybutton.grid(row = 0, column = 1)
     mybutton.place(x = 100, y = 0)
+
 
 def random():
     return
 
+def summary():
+    global mylabel
+    global topic
+    global root
+
+    mylabel.grid_forget()
+    mylabel.destroy()
+            
+    topic.get()
+    #r = requests.get("https://tr.wikipedia.org/wiki/" + topic.get() + "")
+            
+    root.geometry("500x500")
+
+    mylabel = Label(root, text = wikipedia.summary(topic.get(), sentences = 20), wraplength = 400)
+    mylabel.grid(row = 1, column = 0)
+    mylabel.place(x = 50, y = 50)
+
+    exit_button = Button(root, text = "Exit.", command = root.destroy)
+    exit_button.grid(row = 2, column = 0)
+    exit_button.place(x = 400, y = 0)
+
+    topic.delete(0 , END)
 
     
 
