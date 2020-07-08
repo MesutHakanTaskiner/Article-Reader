59a66752181ad905ea542c3429a3d43a9330f39d
HakanTaskinerr <HakanTaskinerr@users.noreply.github.com>
2020-07-08 14:32:50 +0300

    Create reader.py

---
+++ reader.py
@@ -0,0 +1,49 @@
+from tkinter import *
+from PIL import Image, ImageTk
+import requests
+from bs4 import BeautifulSoup
+import wikipedia
+
+root = Tk()
+
+root.title('Wikipedia')
+root.geometry("350x40")
+
+mylabel = Label(root, text = "")
+
+def summary():
+    global mylabel
+
+    mylabel.grid_forget()
+    mylabel.destroy()
+    
+    topic.get()
+    #r = requests.get("https://tr.wikipedia.org/wiki/" + topic.get() + "")
+    
+    root.geometry("500x500")
+    mylabel = Label(root, text = wikipedia.summary(topic.get(), sentences = 20), wraplength = 400)
+    mylabel.grid(row = 1, column = 0)
+    mylabel.place(x = 50, y = 50)
+
+    topic.delete(0 , END)
+    #for i in range(1):
+    #    mylabel = Label(root, text = wikipedia.summary(topic.get(), sentences = i))
+    #    mylabel.grid(row = 1, column = 0, columnspan = 1)
+        
+
+#r = requests.get("https://tr.wikipedia.org/wiki/Google")
+
+#soup = BeautifulSoup(r.content, "html.parser")
+
+topic = Entry(root, width = 20)
+topic.grid(row = 0, column = 0, ipady = 5)
+topic.place(x = 0, y = 3)
+
+mybutton = Button(root, text = "Enter your topic what dou you want to read", command = summary)
+mybutton.grid(row = 0, column = 1)
+mybutton.place(x = 100, y = 0)
+
+#print(wikipedia.summary('Matematik', sentences = 3))
+
+
+root.mainloop()
